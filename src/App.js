df513ded55bea4b0fb48a36b9d1e005aa197d113
Zack <aminuzack7@gmail.com>
2021-01-17 13:03:43 +0100

    deploying

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import {ChakraProvider,theme} from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 
 function App() {
